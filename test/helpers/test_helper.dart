c5cbee225499e83193cbd12d8c9303fc53835e3d
Deivid Amadeus <deividamadeus1997@gmail.com>
2023-10-26 04:11:13 -0300

    adds WeatherRepositoryImpl and its tests

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,10 +1,12 @@
 import 'package:mockito/annotations.dart';
+import 'package:sky_watch/data/data_sources/remote_data_source.dart';
 import 'package:sky_watch/domain/repositories/weather_repository.dart';
 import 'package:http/http.dart' as http;
 
 @GenerateMocks(
   [
     WeatherRepository,
+    WeatherRemoteDataSource,
   ],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
 )
